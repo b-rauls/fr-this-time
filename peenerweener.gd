d83d14654744212801341c772f23bd6ed4df5f6a
mymidimelody <melodym0725@icloud.com>
2025-03-23 02:19:41 -0400

    Added dash, variable height jump, run, acceleration and deceleration prototype mechanics.

--- peenerweener.gd
+++ peenerweener.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -900.0
+@export var walk_speed = 200.0
+@export var run_speed = 300.0
+@export_range(0,1) var walk_acceleration = 0.2
+@export_range(0,1) var run_acceleration = 0.03
+@export_range(0,1) var deceleration = 0.14
+
+@export var jump_force = -900.0
+@export_range(0,1) var decelerate_on_jump_release = 0.5
+
+@export var dash_speed = 500.0
+@export var dash_max_distance = 100.0
+@export var dash_curve: Curve
+@export var dash_cooldown = 1.0
+
+var is_dashing = false
+var dash_start_position = 0
+var dash_direction = 0
+var dash_timer = 0
 
 @onready var _animation_player = $AnimationPlayer
 
@@ -17,18 +33,42 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction !=0:
-		velocity.x = direction * SPEED
-		$Sprite2D.flip_h=direction <0
+		if Input.is_action_pressed("run") and abs(velocity.x) > 0:
+			velocity.x = move_toward(velocity.x, direction * run_speed, run_speed * run_acceleration)
+		elif Input.is_action_pressed("run"):
+			velocity.x = move_toward(velocity.x, direction * run_speed, run_speed * run_acceleration * 2)
+		else:
+			velocity.x = move_toward(velocity.x, direction * walk_speed, walk_speed * walk_acceleration)
+		$Sprite2D.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, 20)
+		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
 		
-		
+	# Dash activation
+	if Input.is_action_just_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
+		is_dashing = true
+		dash_start_position = position.x
+		dash_direction = direction
+		dash_timer = dash_cooldown
+	
+	# Performs actual dash
+	if is_dashing:
+		var current_distance = abs(position.x - dash_start_position)
+		if current_distance >= dash_max_distance or is_on_wall():
+			is_dashing = false
+		else:
+			velocity.x = dash_direction * dash_speed * dash_curve.sample(current_distance / dash_max_distance)
+			velocity.y = 0
+	
+	if dash_timer > 0:
+		dash_timer -= delta
+	
 	move_and_slide()
 	
